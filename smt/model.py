985c56f98014e566e64eb2d6f6b40e7af0f72822
Giorgio Buzzanca <giorgiobuzzanca@outlook.it>
2023-01-23 20:44:07 +0100

    smt model updated with cumulative constraint and symmetry breaking ones

--- smt/model.py
+++ smt/model.py
@@ -2,6 +2,32 @@ import typing
 import json
 
 
+def lexicographic_ordering_ror(aux_vars_prefix: str, vector_a: typing.List[str], vector_b: typing.List[str]) -> str:
+    aux_vars = [aux_vars_prefix + str(i) for i in range(len(vector_a))]
+    aux_vars_definition = ""
+    n = len(vector_a)
+    for i in range(n):
+        aux_vars_definition += "(declare-fun {}{} () Bool)\n".format(aux_vars_prefix, i)
+
+    constraints = "(assert {})\n".format(aux_vars[0])
+    constraints += "(assert " + \
+                   iff("{}".format(aux_vars[n - 1]), "(<= {} {})".format(vector_a[n - 1], vector_b[n - 1])) + \
+                   ")\n"
+
+    for i in range(1, n):
+        constraints += "(assert " + \
+                       iff(aux_vars[n - i - 1], "(or (< {} {}) (and (= {} {}) {}))".format(
+                           vector_a[n - i - 1], vector_b[n - i - 1],
+                           vector_a[n - i - 1], vector_b[n - i - 1],
+                           aux_vars[n - i])) + ")\n"
+
+    return aux_vars_definition + "\n\n" + constraints
+
+
+def iff(a: str, b: str) -> str:
+    return "(and (=> {} {}) (=> {} {}))".format(a, b, b, a)
+
+
 class Model:
     def __init__(self, n_circuits: int, board_width: int, widths: typing.List[int], heights: typing.List[int],
                  height_lower_bound: int, height_upper_bound: int, time_limit_ms: int):
@@ -22,7 +48,7 @@ class Model:
         return Model(**instance, time_limit_ms=time_limit_ms)
 
     def _get_smt_lib_options(self) -> str:
-        return "(set-option :produce-models true)\n(set-option :timeout {})\n(set-logic QF_LIA)\n".\
+        return "(set-option :produce-models true)\n(set-option :timeout {})\n(set-logic QF_LIA)\n". \
             format(self.time_limit_ms)
 
     def _declare_smt_lib_variables(self) -> str:
@@ -32,51 +58,32 @@ class Model:
 
         return variables_declaration
 
-    def _declare_smt_lib_constants(self) -> str:
-        constants_declaration = "(declare-const n_circuits Int)\n" \
-                                "(declare-const board_width Int)\n" \
-                                "(declare-const height_lower_bound Int)\n" \
-                                "(declare-const height_upper_bound Int)\n"
-        for i in range(self.n_circuits):
-            constants_declaration += "(declare-const w{} Int)\n(declare-const h{} Int)\n".format(i, i)
-
-        return constants_declaration
-
-    def _get_constants_assertion(self) -> str:
-        constants_assertions = "(assert (= n_circuits {}))\n" \
-                               "(assert (= board_width {}))\n" \
-                               "(assert (= height_lower_bound {}))\n" \
-                               "(assert (= height_upper_bound {}))\n".\
-            format(self.n_circuits, self.board_width, self.height_lower_bound, self.height_upper_bound)
-        for i in range(self.n_circuits):
-            constants_assertions += "(assert (= w{} {}))\n(assert (= h{} {}))\n".\
-                format(i, self.widths[i], i, self.heights[i])
-
-        return constants_assertions
-
     def _impose_variables_domain(self) -> str:
         variables_domain_assertions = ""
         for i in range(self.n_circuits):
             variables_domain_assertions += "(assert (>= x{} 0))\n" \
                                            "(assert (>= y{} 0))\n" \
-                                           "(assert (<= x{} (- board_width w{})))\n" \
-                                           "(assert (<= y{} (- height_upper_bound h{})))\n".format(i, i, i, i, i, i)
+                                           "(assert (<= x{} (- {} {})))\n" \
+                                           "(assert (<= y{} (- {} {})))\n".format(i, i, i, self.board_width,
+                                                                                  self.widths[i], i,
+                                                                                  self.height_upper_bound,
+                                                                                  self.heights[i])
 
         return variables_domain_assertions
 
     def _impose_effective_board_height(self) -> str:
         effective_board_height_constraint = ""
         for i in range(self.n_circuits):
-            effective_board_height_constraint += "(assert (<= y{} (- board_height h{})))\n".format(i, i)
+            effective_board_height_constraint += "(assert (<= y{} (- board_height {})))\n".format(i, self.heights[i])
 
         return effective_board_height_constraint
 
-    @staticmethod
-    def _non_overlapping_constraint(i: int, j: int) -> str:
+    def _non_overlapping_constraint(self, i: int, j: int) -> str:
         # x[i] + dx[i] <= x[j] \/ y[i] + dy[i] <= y[j] \/
         # x[j] + dx[j] <= x[i] \/ y[j] + dy[j] <= y[i]
-        return "(assert (or (<= (+ x{} w{}) x{}) (<= (+ y{} h{}) y{}) " \
-               "(<= (+ x{} w{}) x{}) (<= (+ y{} h{}) y{})))\n".format(i, i, j, i, i, j, j, j, i, j, j, i)
+        return "(assert (or (<= (+ x{} {}) x{}) (<= (+ y{} {}) y{}) " \
+               "(<= (+ x{} {}) x{}) (<= (+ y{} {}) y{})))\n".format(i, self.widths[i], j, i, self.heights[i], j,
+                                                                    j, self.widths[j], i, j, self.heights[j], i)
 
     def _allowable_circuits_positions(self, vertical: bool = False) -> str:
         allowable_circuits_positions_constraints = ""
@@ -86,8 +93,8 @@ class Model:
             allowable_circuits_positions_constraints += "(= {}{} 0) ".format(variable, i)
             for j in range(self.n_circuits):
                 if i != j:
-                    allowable_circuits_positions_constraints += "(= (+ {}{} w{}) {}{}) ".\
-                        format(variable, j, j, variable, i)
+                    allowable_circuits_positions_constraints += "(= (+ {}{} {}) {}{}) ". \
+                        format(variable, j, self.widths[j], variable, i)
 
             allowable_circuits_positions_constraints += "))\n"
 
@@ -125,19 +132,63 @@ class Model:
 
         return constraints
 
-    def to_smt_lib_format(self) -> str:
-        smt_lib_instance = self._get_smt_lib_options() + "\n" + self._declare_smt_lib_constants() + "\n" + \
-                           self._declare_smt_lib_variables() + "\n" + self._get_constants_assertion() + "\n" + \
-                           self._impose_variables_domain()
+    def _large_circuits_pair(self):
+        constraints = ""
+        for i in range(self.n_circuits):
+            for j in range(i + 1, self.n_circuits):
+                if self.widths[i] + self.widths[j] > self.board_width:
+                    constraints += "(assert (or (<= (+ y{} {}) y{}) (>= y{} (+ y{} {}))))\n". \
+                        format(i, self.heights[i], j, i, j, self.heights[j])
+                if self.heights[i] + self.heights[j] > self.height_upper_bound:
+                    constraints += "(assert (or (<= (+ x{} {}) x{}) (>= x{} (+ x{} {}))))\n". \
+                        format(i, self.widths[i], j, i, j, self.widths[j])
+
+        return constraints
+
+    def lexicographic_ordering_symmetry_breaking(self):
+        constraints = ""
+        for i in range(self.n_circuits):
+            reflected_x_positions = ["(- {} {} {})".format(self.board_width, self.widths[i], "x{}".format(i)) for i in
+                                     range(self.n_circuits)]
+            reflected_y_positions = ["(- {} {} {})".format(self.height_upper_bound, self.heights[i], "y{}".format(i))
+                                     for i in range(self.n_circuits)]
+            constraints += lexicographic_ordering_ror("aux_lex_x_{}_".format(i),
+                                                      ["x{}".format(i) for i in range(self.n_circuits)],
+                                                      reflected_x_positions)
+            constraints += lexicographic_ordering_ror("aux_lex_y_{}_".format(i),
+                                                      ["y{}".format(i) for i in range(self.n_circuits)],
+                                                      reflected_y_positions)
+
+        return constraints
+
+    def one_pair_of_circuits_symmetry_breaking(self):
+        areas = [(i, self.widths[i] * self.heights[i]) for i in range(self.n_circuits)]
+        areas.sort(key=lambda x: x[1], reverse=True)
+        o1 = areas[1][0]
+        o2 = areas[2][0]
+
+        return lexicographic_ordering_ror("aux_lex_sb_", ["y{}".format(o1), "x{}".format(o1)],
+                                          ["y{}".format(o2), "x{}".format(o2)])
+
+    def to_smt_lib_format(self, turn_on_cumulative_constraints: bool = False,
+                          turn_on_symmetry_breaking: bool = True) -> str:
+        smt_lib_instance = self._get_smt_lib_options() + "\n" + self._declare_smt_lib_variables() + "\n" + \
+                           self._impose_variables_domain() + "\n"
 
         for i in range(self.n_circuits):
             for j in range(i + 1, self.n_circuits):
                 smt_lib_instance += self._non_overlapping_constraint(i, j)
 
-        # smt_lib_instance += self._allowable_circuits_positions()
-        # smt_lib_instance += self._allowable_circuits_positions(vertical=True)
-        smt_lib_instance += self.cumulative_constraints_x()
-        smt_lib_instance += self.cumulative_constraints_y()
+        smt_lib_instance += self._large_circuits_pair()
+
+        if turn_on_cumulative_constraints:
+            smt_lib_instance += self.cumulative_constraints_x()
+            smt_lib_instance += self.cumulative_constraints_y()
+
+        if turn_on_symmetry_breaking:
+            smt_lib_instance += self.lexicographic_ordering_symmetry_breaking()
+            smt_lib_instance += self.one_pair_of_circuits_symmetry_breaking()
+
         smt_lib_instance += self._impose_effective_board_height()
         smt_lib_instance += "(assert (= board_height {}))\n".format(self.height_lower_bound)
         smt_lib_instance += "(check-sat)\n(get-model)\n"
